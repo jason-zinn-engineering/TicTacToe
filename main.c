9ccfbd033226fdbdddce39dd3df10ed465ea1c85
Jason Zinn <jason.zinn@ucalgary.ca>
2019-04-01 14:09:48 -0600

    Step 3

--- main.c
+++ main.c
@@ -9,22 +9,54 @@ char board[3][3] = {
     {'|','|','|'},
     {'|','|','|'}
 };
-/* Define Board Print Function */
-void boardPrinter(char board[3][3])
+/* Define Repeat Instructions */
+void instruct()
 {
-    printf(board[0]);
-    printf("\n");
-    printf(board[1]);
-    printf("\n");
-    printf(board[2]);
-    printf("\n");
+    printf("[1][2][3]\n[4][5][6]\n[7][8][9]\nThese are the keys that you will use to pick your position\n");
 }
+/* Return a pointer to mutate board location */
+char* boardLocation(char input)
+{
+    switch (input) {
+        case '1':
+            return &board[0][0];
+            break;
+        case '2':
+            return &board[0][1];
+            break;
+        case '3':
+            return &board[0][2];
+            break;
+        case '4':
+            return &board[1][0];
+            break;
+        case '5':
+            return &board[1][1];
+            break;
+        case '6':
+            return &board[1][2];
+            break;
+        case '7':
+            return &board[2][0];
+            break;
+        case '8':
+            return &board[2][1];
+            break;
+        case '9':
+            return &board[2][2];
+            break;
+        default:
+            return &board[1][1];
+            break;
+    }
+}
+
+/*  */
 
 int main()
 {
     printf("~ Welcome to jason-zinn-engineering Tic Tac Toe ~\n");
     printf("-------------------------------------------------\n");
-    printf("[1][2][3]\n[4][5][6]\n[7][8][9]\n");
-    boardPrinter(board);
+    instruct();
     return 0;
 }
