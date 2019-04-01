11924d9eb9461ca052866dc748ab135929664f12
Jason Zinn <jason.zinn@ucalgary.ca>
2019-04-01 12:24:45 -0600

    Step 1

--- main.c
+++ main.c
@@ -2,3 +2,13 @@
 @Author Jason Zinn
 @Date April 1st, 2019
 */
+
+
+char board[3][3];
+
+int main()
+{
+    printf("~ Welcome to jason-zinn-engineering Tic Tac Toe ~\n");
+    printf("-------------------------------------------------");
+    return 0;
+}
