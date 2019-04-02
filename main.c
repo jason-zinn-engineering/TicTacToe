cd2d70e52af462384c5c785957340a09fc10508e
Jason Zinn <jason.zinn@ucalgary.ca>
2019-04-02 02:00:46 -0600

    Architecting

--- main.c
+++ main.c
@@ -1,21 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
 /*
+ ***********************************************************************************************************************
  @Author Jason Zinn
  @Date April 1st, 2019
  *This is a simple Tic Tac Toe Game Written in C.
+ ***********************************************************************************************************************
  */
-#include <stdio.h>
-#include <stdlib.h>
-# include <string.h>
+/*
+ ***********************************************************************************************************************
+ File Architecture
+ ***********************************************************************************************************************
+ */
+int main(void);
+void instruct(void);
+void printBoard(void);
+void changeBoard(char);
+char *boardLocation(char);
+/*
+ ***********************************************************************************************************************
+ VARIABLES
+ 1. Matrix Array board
+ 2. Matrix location ptrs
+ ***********************************************************************************************************************
+ */
 /* Define Board */
 char board[3][3] = {
     {'|','|','|'},
     {'|','|','|'},
     {'|','|','|'}
 };
+/* Create Syntactic Sugar for Array Locations */
+char *one = &board[0][0];
+char *two = &board[0][1];
+char *three = &board[0][2];
+char *four = &board[1][0];
+char *five = &board[1][1];
+char *six = &board[1][2];
+char *seven = &board[2][0];
+char *eight = &board[2][1];
+char *nine = &board[2][2];
+/*
+ ***********************************************************************************************************************
+ FUNCTIONS
+ 1. instruct => Print Directions
+ 2. changeBoard => @params char input -
+ 3. gameRequest => Scan @locate -
+ ***********************************************************************************************************************
+ */
 /* Define Repeat Instructions */
 void instruct()
 {
+    printf("~ Welcome to jason-zinn-engineering Tic Tac Toe ~\n");
+    printf("-------------------------------------------------\n");
     printf("[1][2][3]\n[4][5][6]\n[7][8][9]\nThese are the keys that you will use to pick your position\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("START!\n");
+    
+}
+/* Take Tic Tac Toe Pointer and apply */
+void changeBoard(char input)
+{
+    char* temp = boardLocation(input);
+    *temp = 'X';
 }
 /* Here We Print The Board from Reference */
 void printBoard()
@@ -27,16 +76,18 @@ void printBoard()
         }
     }
 }
-/* Create Syntactic Sugar for Array Locations */
-char *one = &board[0][0];
-char *two = &board[0][1];
-char *three = &board[0][2];
-char *four = &board[1][0];
-char *five = &board[1][1];
-char *six = &board[1][2];
-char *seven = &board[2][0];
-char *eight = &board[2][1];
-char *nine = &board[2][2];
+/* Player Turn */
+void gameRequest()
+{
+    char locate;
+    printf("Select a location => ");
+    scanf("%c", &locate);
+    changeBoard(locate);
+    printBoard();
+    printf("\n-------------------------------------------------\n");
+    printf("Computer Turn");
+    
+}
 /* Return a pointer to mutate board location */
 char* boardLocation(char input)
 {
@@ -76,24 +127,7 @@ char* boardLocation(char input)
             break;
     }
 }
-/* Take Tic Tac Toe Pointer and apply */
-void changeBoard(char input)
-{
-    char* temp = boardLocation(input);
-    *temp = 'X';
-}
-/* Game Function */
-void gameRequest()
-{
-    char locate;
-    printf("Select a location => ");
-    scanf("%c", &locate);
-    changeBoard(locate);
-    printBoard();
-    printf("\n-------------------------------------------------\n");
-    printf("Computer Turn");
-    
-}
+
 /* Check for a Winner */
 int checkWinner(char board[3][3])
 {
@@ -214,7 +248,7 @@ void writer(char board[3][3])
         }
     }
     /* Write to File and Close */
-    fprintf(filePointer, "%s", writeable);
+    fprintf(filePointer, "%s", "Lake Boyz");
     fclose(filePointer);
     fflush(filePointer);
     
@@ -222,14 +256,17 @@ void writer(char board[3][3])
 /* This is our main return function of our program */
 int main()
 {
+    /*
+     ** Multi-threading
+     */
+    
+    /*
+     ** Multi-threading STOP
+     */
     /* File Manipulation */
     writer(board);
     /* Tic Tac Toe Game */
-    printf("~ Welcome to jason-zinn-engineering Tic Tac Toe ~\n");
-    printf("-------------------------------------------------\n");
     instruct();
-    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("START!\n");
     gameLoop();
     return 0;
 }
